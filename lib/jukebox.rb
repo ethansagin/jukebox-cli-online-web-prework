af7b6c993c2536f597525ba36912ff2fe759ee88
Ethan <ethansagin@gmail.com>
2019-03-24 21:01:26 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,4 @@
+require 'pry'
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -10,3 +11,67 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  puts "I accept the following commands:
+        - help : displays this help message
+        - list : displays a list of songs you can play
+        - play : lets you choose a song to play
+        - exit : exits this program"
+end
+
+def list(songs_arr)
+  counter = 0
+  until counter == songs_arr.length
+    puts "#{counter + 1}. #{songs_arr[counter]}"
+    counter += 1
+  end
+end
+  
+def play(songs_arr)
+  puts "Please enter a song name or number:"
+  input = gets.chomp
+  if (1..9).include?(input.to_i)
+    puts "Playing #{songs_arr[input.to_i - 1]}"
+  elsif songs_arr.include?(input)
+    puts "Playing #{input}"
+  else
+    puts "Invalid input, please try again"
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def prompt
+  puts "Please enter a command:"
+  gets.chomp
+end
+  
+def run(songs_arr)
+  help
+  puts "Please enter a command:"
+  input = gets.chomp
+  
+  while input != "exit"
+    if input == "list"
+      list(songs_arr)
+      puts "Please enter a command:"
+      input
+    elsif input == "play"
+      play(songs_arr)
+      puts "Please enter a command:"
+      input
+      elsif input == "help"
+      help
+      puts "Please enter a command:"
+      input
+      else
+      puts "Invalid command"
+      help
+      puts "Please enter a command:"
+      input
+    end
+  end
+  exit_jukebox
+end
